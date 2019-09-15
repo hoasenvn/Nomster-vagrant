2f8590666cb47045b417438636ea33aea9b693aa
Kevin <vuong.kevin@ymail.com>
2019-09-15 21:18:06 +0000

    38th commit: Lesson 41

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,18 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.action_mailer.default_url_options = { host: 'nomster-trucnguyen.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  config.action_mailer.default_url_options = { host: 'localhost:3030' } # replaced 'nomster-trucnguyen.herokuapp.com' by 'localhost:3030'
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -32,7 +43,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true # replaced false by true
 
   config.action_mailer.perform_caching = false
 
